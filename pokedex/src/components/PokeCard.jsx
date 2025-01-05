41b0560cd9540f2fd81b4e7ca4df2a68cd6f6c08
Valentino Ivanovski <valentino.ivanovski@icloud.com>
2025-01-05 01:56:27 +0100

    One step before fetching the pokemon data

--- pokedex/src/components/PokeCard.jsx
+++ pokedex/src/components/PokeCard.jsx
@@ -1,4 +1,34 @@
-export function PokeCard(){
+import {useEffect} from 'react'
+
+export function PokeCard(props){
+    const {selectedPokemon} = props
+
+    const [data, setData] = useState(null)
+    const [loading, setLoading] = useState(false)
+
+    useEffect(() => {
+
+        // if loading, exit logic
+        if (loading || !localStorage) {return}
+        // check if the selected pokemon information is available in the cache
+            // 1. define the cache
+            let cache = {}
+            if (localStorage.getItem('pokedex')) {
+                cache = JSON.parse(localStorage.getItem('pokemon'))
+            }
+            // 2. check if the selected pokemon is in the cache, otherwise fetch from the API
+            if (selectedPokemon in cache) {
+                setData(cache[selectedPokemon]) //we read from the cache to avoid making unnecessary API calls
+                return
+            }
+
+        // fetch the selected pokemon information from the API
+        
+            
+        // if we fetch from the api, make sure to save the information to the cache for next time
+
+    }, [selectedPokemon])
+
     return(
         <div></div>
     )
