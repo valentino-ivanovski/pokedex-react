84006b607d68b9c39a64e9488f4d3a07fcc25d2d
Valentino Ivanovski <valentino.ivanovski@icloud.com>
2025-01-05 01:22:00 +0100

    Showed Pokemon on the sidebar

--- pokedex/src/components/SideNav.jsx
+++ pokedex/src/components/SideNav.jsx
@@ -1,11 +1,17 @@
 import {first151Pokemon} from "../utils"
+import {getFullPokedexNumber} from "../utils"
 
 export function SideNav(){
     return(
         <nav>
+            <div className={"header"}>
+                <h1 className="text-gradient">Pokédex</h1>
+            </div>
+            <input />
             {first151Pokemon.map((pokemon, pokemonIndex) => {
                 return(
-                    <button>
+                    <button className={'nav-card'}>
+                        <p>{getFullPokedexNumber(pokemonIndex)}</p>
                         <p>{pokemon}</p>
                     </button>
                 )
